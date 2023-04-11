7e1edfb21e2f21415e857570844e8009b9817751
diegobirus <di_almeida@outlook.com>
2023-04-10 20:01:27 -0700

    Flapping Bird

---
+++ jogo.js
@@ -0,0 +1,13 @@
+console.log('Flappy Bird');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.drawImage ( //drawImage(image: CanvasImageSource, dx: number, dy: number): void
+
+  
+
+)
